5203e5b7bd20e7e22ae1224c93756e708dd8466d
sssvint <93766121+sssvint@users.noreply.github.com>
2021-12-10 11:45:51 +0200

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,23 @@
+$url = 'https://my.e-klase.lv';
+$fields = array(
+            //post parameters to be sent to the other website
+            'text'=>urlencode($_POST['text']), //the post request you send to this  script from your domain.
+        );
+
+//url-ify the data for the POST
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string,'&');
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_POST,count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+
+//execute post
+$result = curl_exec($ch);
+
+//close connection
+curl_close($ch);
